9786766447e03d6c33c530b36b2b4e19aa18ab54
wascoyur <mytem20007@gmail.com>
2020-03-18 21:41:22 +0500

    выпадающий список реализован

--- js.js
+++ js.js
@@ -1,9 +1,36 @@
-inputCitiesTo = document.querySelector('.form-search');
-inputCitiesTo = document.querySelector('.input__cities-from');
-inputCitiesTo = document.querySelector('.dropdown__cities-to');
-inputCitiesTo = document.querySelector('.dropdown__cities-from');
-inputCitiesTo = document.querySelector('.input__cities-to');
-inputCitiesTo = document.querySelector('input__date-depart');
-inputCitiesTo = document.querySelector('input__date-depart');
+const formSearch = document.querySelector('.form-search'),
+inputCitiesFrom = document.querySelector('.input__cities-from'),
+dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+dropdownCitiesfrom= document.querySelector('.dropdown__cities-from'),
+inputCitiesTo = document.querySelector('.input__cities-to'),
+inputDateDepart = document.querySelector('input__date-depart');
 
-const city = ['Москва', 'Санкт-Петербург', 'Минск Караганда'];
+const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'новгород','Калининград',
+'Волгоград', 'Самара', 'Керчь'];
+
+const showCity = (input, list) => {
+  list.textContent = "";
+
+  if (input.value !== "") {
+    const filterCity = city.filter((item, i, arr) => {
+      const it = item.toLowerCase();
+      let r = it.includes(input.value.toLowerCase());
+      return r;
+    });
+
+    filterCity.forEach(item => {
+      const li = document.createElement("li");
+      li.classList.add("dropdown__city");
+      li.textContent = item;
+      list.append(li);
+      console.log(li);
+    });
+  }
+};
+
+
+inputCitiesFrom.addEventListener('input', ()=>{
+    showCity(inputCitiesFrom, dropdownCitiesfrom)
+})
+
+

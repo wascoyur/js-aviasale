b0ac548e094248d779e1b7c7431926208f582db0
wascoyur <mytem20007@gmail.com>
2020-03-18 18:50:07 +0500

    добавлены переменные формы

---
+++ js.js
@@ -0,0 +1,9 @@
+inputCitiesTo = document.querySelector('.form-search');
+inputCitiesTo = document.querySelector('.input__cities-from');
+inputCitiesTo = document.querySelector('.dropdown__cities-to');
+inputCitiesTo = document.querySelector('.dropdown__cities-from');
+inputCitiesTo = document.querySelector('.input__cities-to');
+inputCitiesTo = document.querySelector('input__date-depart');
+inputCitiesTo = document.querySelector('input__date-depart');
+
+const city = ['Москва', 'Санкт-Петербург', 'Минск Караганда'];
